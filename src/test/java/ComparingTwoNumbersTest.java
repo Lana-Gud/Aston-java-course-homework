8a190394a467ce05774c1b07ab07eec12bfc6ad1
Lana-Gud <lana.gudkova.1999@bk.ru>
2026-01-20 18:51:20 +0400

    Update ComparingTwoNumbersTest.java
    убрала лишние "/"

--- src/test/java/ComparingTwoNumbersTest.java
+++ src/test/java/ComparingTwoNumbersTest.java
@@ -5,18 +5,18 @@ public class ComparingTwoNumbersTest {
     @Test
     public void testCompareEqual() {
         ComparingTwoNumbers comparator = new ComparingTwoNumbers();
-        assertEquals(comparator.compare(5, 5), \"Значения 5 и 5 равны\");
+        assertEquals(comparator.compare(5, 5), "Значения 5 и 5 равны");
     }
     
     @Test
     public void testCompareLess() {
         ComparingTwoNumbers comparator = new ComparingTwoNumbers();
-        assertEquals(comparator.compare(3, 7), \"Первое значение (3) меньше второго (7)\");
+        assertEquals(comparator.compare(3, 7), "Первое значение (3) меньше второго (7)");
     }
     
     @Test
     public void testCompareGreater() {
         ComparingTwoNumbers comparator = new ComparingTwoNumbers();
-        assertEquals(comparator.compare(10, 2), \"Первое значение (10) больше второго (2)\");
+        assertEquals(comparator.compare(10, 2), "Первое значение (10) больше второго (2)");
     }
 }
